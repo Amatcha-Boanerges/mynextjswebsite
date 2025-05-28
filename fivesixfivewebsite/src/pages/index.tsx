01b47fbe67af8e15699e3418db81959bbbc7ca4b
jacques.y.b. <jacquesyann@live.fr>
2025-05-28 21:50:47 +0000

    feat: Verify Tailwind CSS setup

--- fivesixfivewebsite/src/pages/index.tsx
+++ fivesixfivewebsite/src/pages/index.tsx
@@ -25,6 +25,7 @@ export default function Home() {
           height={38}
           priority
         />
+        <p className="text-red-500">This text should be red if Tailwind is working.</p>
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
